806761b0d558f00ef1df59240096bd2b0a26a644
Simon Borg <sci-fi101@hotmail.com>
2024-05-27 15:16:30 +0200

    Added basic Logger, will add Nlog later.

---
+++ Code/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace Online_Pet_Store_Example;
+
+public static class Logger
+{
+    public static void Log(string text)
+    {
+        Console.WriteLine(text);
+        Debug.WriteLine(text);
+    }
+}
